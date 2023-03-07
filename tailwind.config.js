bb9d0b18a7a27faa082422c3080628ff43189099
mike mambwe <mikeapple056@gmail.com>
2023-03-07 13:59:46 +0200

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:"class",
+  content: ["./src/**/*.{html,js}",
+  "./components/**/*.{js.ts.jsx,tsx}",
+  "./pages/**/*.{js,ts,jsx.tsx}",
+
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
