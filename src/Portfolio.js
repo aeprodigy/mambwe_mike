fba2da82c2903a6d6a76685787cc9a6139021a55
mike mambwe <mikeapple056@gmail.com>
2023-03-07 14:06:18 +0200

    Portfolio.js

---
+++ src/Portfolio.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div className='h-screen'>
+      <section>
+        <div className=''>
+          <h3 className="text-3xl py-1">Portfolio</h3>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Portfolio
