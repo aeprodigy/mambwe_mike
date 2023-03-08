8265ebf90654371b3c0df0eb144883fd00ced2e6
mike mambwe <mikeapple056@gmail.com>
2023-03-08 19:36:23 +0200

    footer

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='text-center pt-4 text-gray-500 text-bold dark:text-white'>
+      <p>Copyright © Mike Mambwe 2022</p>
+    </div>
+  );
+}
+
+export default Footer
