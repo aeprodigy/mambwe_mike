a0b98e12599f85800c4740f414310b17dbca1997
mike mambwe <mikeapple056@gmail.com>
2023-03-09 17:53:22 +0200

    dark

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-center pt-4 text-gray-500 text-bold dark:text-white'>
-      <p>Copyright © Mike Mambwe 2022</p>
+    <div className='text-center pt-4 font-600 text-gray-500 text-bold dark:text-white dark:bg-black'>
+      <p>© Mike Mambwe 2022</p>
     </div>
   );
 }
